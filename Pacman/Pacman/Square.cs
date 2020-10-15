b4efb51c58e9c80b0368ae1b3c48dc7b6abce816
Joyce Yeung <joyce.yeung@myob.com>
2020-10-15 12:55:49 +1100

    added square test

--- Pacman/Pacman/Square.cs
+++ Pacman/Pacman/Square.cs
@@ -1,7 +1,26 @@
+using System;
+
 namespace Pacman
 {
     public class Square
     {
-        
+        private bool _wall;
+        private bool _wall2;
+
+        public Square(bool isWall, bool isWall2)
+        {
+            _wall = isWall;
+            _wall2 = isWall2;
+        }
+
+        public bool IsWall()
+        {
+            return _wall;
+        }
+
+        public bool IsRightBorderAWall()
+        {
+            return true;
+        }
     }
 }
--- Pacman/PacmanTests/SquareTest.cs
+++ Pacman/PacmanTests/SquareTest.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using Pacman;
+using Xunit;
+
 namespace PacmanTests
 {
     public class SquareTest
     {
         public SquareTest()
         {
+        }
+
+        [Fact]
+        public void ShouldReturnTrue_WhenTopOfSqaureIsAWall()
+        {
+            var square = new Square(true, true);
+            var result = square.IsWall();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldReturnFalse_WhenTopOfSquareIsNotAWall()
+        {
+            var square = new Square(false,true);
+            var result = square.IsWall();
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ShouldReturnTrue_WhenRightOfSquareIsAWall()
+        {
+            var square = new Square(false,true);
+            var result = square.IsRightBorderAWall();
+
+            Assert.True(result);
         }
     }
 }
