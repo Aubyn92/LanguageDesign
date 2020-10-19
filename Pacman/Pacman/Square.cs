107a1fa715e1def84e744bf419e25d6bf34af28c
Aubyn92 <livstaubyn92@gmail.com>
2020-10-19 15:41:42 +1100

    changed square class to include bottom and left walls

--- Pacman/Pacman/Square.cs
+++ Pacman/Pacman/Square.cs
@@ -4,23 +4,49 @@ namespace Pacman
 {
     public class Square
     {
-        private bool _wall;
-        private bool _wall2;
+        private bool _topBorder;
+        private bool _rightBorder;
+        private bool _bottomBorder;
+        private bool _leftBorder;
+        private bool _dot;
 
-        public Square(bool isWall, bool isWall2)
+        public Square(bool topBorder, bool rightBorder, bool bottomBorder, bool leftBorder)
         {
-            _wall = isWall;
-            _wall2 = isWall2;
+            _topBorder = topBorder;
+            _rightBorder = rightBorder;
+            _bottomBorder = bottomBorder;
+            _leftBorder = leftBorder;
+            _dot = true;
         }
 
-        public bool IsWall()
+        public bool IsTopBorderAWall()
         {
-            return _wall;
+            return _topBorder;
         }
 
         public bool IsRightBorderAWall()
         {
-            return true;
+            return _rightBorder;
+        }
+
+        public bool IsBottomBorderAWall()
+        {
+            return _bottomBorder;
+        }
+
+        public bool IsLeftBorderAWall()
+        {
+            return _leftBorder;
+        }
+
+        public bool CanRemoveDot()
+        {
+            if (_dot == true)
+            {
+                _dot = false;
+                return true;
+            }
+            return false;
         }
     }
 }
